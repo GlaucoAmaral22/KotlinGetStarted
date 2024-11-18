98a123401e948845d20066dd41753f3959ad62b1
Glauco Amaral Geraldino <glauco1998@gmail.com>
2024-11-18 11:53:23 -0300

    adicionando sobre invoke operator

---
+++ src/classes/InvokeMethod.kt
@@ -0,0 +1,29 @@
+package classes
+
+interface BaseUseCase {
+    //o Invoke serve mais para quando tivermos uma unica acao na classe, chamarmos o objeto como methodo ao inves
+    //de precisar colocar um .execute ou .process e tals
+    operator fun invoke()
+}
+
+class FirstUseCase: BaseUseCase {
+    override operator fun invoke() {
+        println("My First UseCase")
+    }
+
+}
+
+class SecondUseCase : BaseUseCase{
+    override operator fun invoke() {
+        println("My Second UseCase")
+    }
+}
+
+fun main() {
+    val firstUseCase = FirstUseCase()
+    val secondUseCase = SecondUseCase()
+    //firstUseCase.execute()
+    //secondUseCase.execute()
+    firstUseCase() //mesma coisa que firstUseCase.invoke()
+    secondUseCase() //mesma coisa que secondUseCase.invoke()
+}
