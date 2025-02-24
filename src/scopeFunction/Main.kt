6752e634646dcc318c4e08bab12f0ff09f664c00
Glauco <glauco1998@gmail.com>
2025-02-24 10:03:12 -0300

    Refatorando os ScopeFunction

--- src/scopeFunction/Main.kt
+++ src/scopeFunction/Main.kt
@@ -1,75 +1,82 @@
 package scopeFunction
 
-data class Person(val name: String, var age: Int, val country: String) {
-    fun incrementAge() = age++
-}
-
+data class Person(var name: String = "Default Value", var age: Int = -1)
 
 fun main() {
+    //Scope function são funções highOrder(recebem/retornam outra função).
+    //Objetivo delas é executar determinadas operações num objeto num escopo.
+    //Cada uma tem o seu caso de uso, mas o intuíto delas é fazer um código: conciso, legível e expressivo.
 
-    //Basicamente, todas essas funções executam a mesma ação: executar um bloco de código em um objeto.
-    // A diferença é como esse objeto fica disponível dentro do bloco e qual é o resultado de toda a expressão.
-
-    //let -> tem o intuito de retornar o resultado do bloco
-    //Usos comuns: verificar nulidade (?.let { ... }), transformar dados ou executar operações que retornam algo.
-    val length = "Kotlin".let {
-        println("O valor do objeto é $it. Ele será retornado")
-        it.length
+    //LET: Executar um bloco de código somente se o objeto é nulo.
+    /**
+     * object?.let {
+     *     // perform operations on the object
+     * }
+     */
+    val message = "Hello World"
+    val result = message.let {
+        val uppercase = message.uppercase()
+        uppercase.reversed()
     }
-    println("O tamanho na palavra acima é $length")
+    println(result)
 
-    val canBeNull = 10
-    val result = canBeNull?.let {
-        println("Não é nula")
-        it + 10
+    val nullable: String? = null
+    nullable?.let { println("Objeto não nulo") }
+
+    //ALSO: Executar algumas operações adicionais sem mudar o estado dele e então retornar o próprio objeto.
+    /**
+     * object.also {
+     *     // perform additional operation on the object
+     * }
+     */
+    val messageAlso = "Hello World!"
+    messageAlso.also {
+        println("The original message is: $it")
+    }.run {
+        uppercase()
+    }.also {
+        println("The uppercase message is: $it")
+        println("Salvando dados no banco...")
     }
-    println("Valor do resultado é $result")
+    println("Valor do messageAlso continua inalterado: $messageAlso")
 
-    //also
-    //retorna o proprio objeto que invocou
-    //Usos comuns: adicionar ações que não modificam o objeto (como log ou validação).
-    "Kotlin".also {
-        println("O tamanho da palavra no also é: ${it.length}")
-        println("O retorno do also sempre será o proprio objeto que está invocando")
-        println("Logando algo do objeto")
-    }
-
-    //run
-    //Usos comuns: inicializar ou processar um objeto de maneira complexa e retornar um resultado específico.
-    val x: Int? = null
-    val resultProcess = x ?: run {
-        println("Executa tal coisa e retorna o objeto")
-        10
-    }
-    println("O Valor do resultProcess é: $resultProcess")
-
-    println("Kotlinnn".run {
-        println("valor objeto: $this")
-        this.length
-    })
-
-    //with
-    //retorna o resultado do bloco de codigo
-    //Diferente das outras, with não é uma função de extensão, então você passa o objeto como argumento.
-    // Ela usa this como o contexto e retorna o resultado do bloco de código.
-    //Usos comuns: realizar várias operações em um objeto existente.
-    val name = "Kotlin"
-    val withResult = with(name) {
-        println("Executando bloco de codigo with")
-        name.length + 3
-    }
-    println("Resultado do with: $withResult")
-
-
-    //apply
-    //A função apply executa um bloco de código no contexto do objeto (this)
-    // e retorna o próprio objeto, o que é útil para inicialização e configuração de objetos.
-    //Usos comuns: inicializar ou configurar objetos (especialmente no padrão builder).
-    Person("Glauco Amaral", 26, "Brasil").apply {
+    //APPLY: Configurar as propriedades de um objeto e retorná-lo.
+    /**
+     * object.apply {
+     *     // configure the properties of the object
+     * }
+     */
+    Person().apply {
+        name = "Glauco Amaral Geraldino"
+        age  = 26
     }
 
-    //apply e also -> retornam o objeto
-    //let, run, with -> retornam o ultimo comando do bloco de codigo
+    val person = Person("Glauco", 26)
+    person.apply {
+        age = 27
+        name = "Amaralzinho"
+    }
+    println("$person")
 
+    //RUN: Executar um bloco de código num objeto e retornar o resultado do bloco
+    /**
+     * object.run {
+     *     // perform the block of code on the object
+     * }
+     */
+    val nameUppercase = person.run {
+        this.name.uppercase()
+    }
+    println(nameUppercase)
 
+    //WITH: Executar um bloco de código num objeto sem a necessidade de chamar o objeto dentro do bloco
+    /**
+     * with(object) {
+     *     // perform the block of code on the object
+     * }
+     */
+    val withResult = with(person) {
+        "Name and Age inside with block: $name, $age"
+    }
+    println(withResult)
 }
